b52ec5e481d02704dd4c8ddbab616c05a484f597
magc <284512272@qq.com>
2021-11-11 10:45:25 +0800

    登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// import Element from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+// Vue.use(Element)
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 导入弹框提示组件需要挂载到vue的原型组件上
+Vue.prototype.$message = Message
